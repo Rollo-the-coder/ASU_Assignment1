5cbf70f1ffc52a0fe195cc53b2d56d47d3655803
Rollo-the-coder <erikgotta@gmail.com>
2021-01-26 20:12:36 +0700

    Updated wording to fit prompt as well as spacing

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,8 @@
 //  CSE 110 Spring 2021
-//  Assignment  :1 Pizza Party
-//  Name        :Erik Gotta
-//  ASU ID      :
-//  Total Time  :
+//  Assignment  : 1 "Pizza Party"
+//  Name        : Erik Gotta
+//  ASU ID      : 1222628953
+//  Total Time  : 60 minutes
 
 import java.util.Scanner;
 
@@ -13,26 +13,26 @@ class Assignment1
         Scanner in = new Scanner(System.in);
 
         //Input Collection
-        System.out.print("Please enter how many pizzas purchased: ");
+        System.out.print("\nNumber of pizzas purchased        : ");
         int pizzasPurchased = in.nextInt();
 
-        System.out.print("Please enter the number of slices per pizza: ");
+        System.out.print("Number of slices per pizza        : ");
         int numberSlices = in.nextInt();
 
-        System.out.print("Please enter the number of adults attending: ");
+        System.out.print("Number of adults                  : ");
         int numberAdults = in.nextInt();
 
-        System.out.print("Please enter the number of children attending: ");
+        System.out.print("Number of children                : ");
         int numberChildren = in.nextInt();
 
-        //Computation
+        //Computation of inputs
         int totalSlices = numberSlices * pizzasPurchased;
         int adultSlices = numberAdults * 2;
         int childrenTotalSlices = totalSlices - adultSlices;
         int childSlicesRecieved = childrenTotalSlices / numberChildren;
         int slicesLeftOver = totalSlices - adultSlices - (childSlicesRecieved * numberChildren);
 
-        //Output
+        //Output Statements
         System.out.println("\nTotal number of slices of pizza               : " + totalSlices);
         System.out.println("Total number of slices given to adults        : " + adultSlices);
         System.out.println("Total number of slices available for children : " + childrenTotalSlices);
